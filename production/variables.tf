206ea24df60e866405d3362117ee8d7608d814a6
asadagaha <dongon74@gmail.com>
2022-11-28 21:33:44 +0900

    merge

--- production/variables.tf
+++ production/variables.tf
@@ -1,18 +1,10 @@
 variable "project" {}
-<<<<<<< HEAD
 variable "region" {}
 variable "web_container_name" {}
 variable "acm_arn" {}
 variable "account_id" {}
 
 locals {
-    env = "prod"
-=======
-variable "web_container_name" {}
-variable "region" {}
-
-locals {
   env        = "prod"
   account_id = "377575712836"
->>>>>>> refs/remotes/origin/main
 }
