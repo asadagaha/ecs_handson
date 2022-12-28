616f59a4d7b74a1f752e3c0e66760199029a9113
asadagaha <dongon74@gmail.com>
2022-12-28 23:16:25 +0900

    backend設定

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 COMMAND=$1
 WORKSPACE=$2
+OPTIONS=${@:3}
 
 terraform workspace select ${WORKSPACE}
-terraform ${COMMAND} -var-file=.terraform/${WORKSPACE}.tfvars
+terraform ${COMMAND} -var-file=.terraform/${WORKSPACE}.tfvars ${OPTIONS}
--- provider.tf
+++ provider.tf
@@ -12,6 +12,8 @@ provider "aws" {
 terraform {
   required_version = "1.3.6"
 
+   backend "s3" {} 
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
